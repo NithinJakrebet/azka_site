34ccc97f9ed71e7e222beede1012a2f63193fe44
Nithin Jakrebet <nithinjbet@gmail.com>
2023-12-27 15:01:16 -0700

    -responsiveness done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import "../styling/footer.css";
+import "../styling/pages.css"
+import fb from "../photos/fb_icon.png"
+import ig from "../photos/Instagram_logo.png"
+import tk from "../photos/tiktok.png"
 
 export default function Footer() {
 	return (
         <div className="logo">
             AZKA
+            <a href="#" target="blank"><img className="rounded-image" src={ fb }></img></a>
+            <a href="#" target="blank"><img className="rounded-image" src={ ig }></img></a>
+            <a href="#" target="blank"><img className="rounded-image" src={ tk }></img></a>
         </div>
     )
 }
+
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,35 +1,45 @@
-/* 
-- Home Page - Upcoming events, pics Slideshow type 
-- Executive committee (intro/bio)
-- Photo gallery (photos by event)
-- Newsletter (yearly)
-- Contact, Zelle
-*/
-
 import React, { useState } from "react";
 import { Link, useMatch, useResolvedPath } from "react-router-dom";
-import "../styling/navbar.css"
+import "../styling/navbar.css";
 
 export default function Navbar() {
-	return <nav className="nav">
-		<Link to="/" className="site-title">Arizona Konkani Association</Link>
-		<ul>
-			<CustomLink to="/">Home</CustomLink>
-			<CustomLink to="/about">About Us</CustomLink>
-			<CustomLink to="/newsletter">Newsletter</CustomLink>
-			<CustomLink to="/contact">Contact</CustomLink>
-		</ul>
-	</nav>
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
+    return (
+        <nav className="nav">
+            <Link to="/" className="site-title">Arizona Konkani Association</Link>
+            <div className="hamburger-menu" onClick={toggleMenu}>☰</div>
+            <ul className={`dropdown ${isMenuOpen ? "active" : ""}`}>
+                {/* Dropdown links */}
+                <CustomLink to="/" onClick={toggleMenu}>Home</CustomLink>
+                <CustomLink to="/about" onClick={toggleMenu}>About Us</CustomLink>
+                <CustomLink to="/newsletter" onClick={toggleMenu}>Newsletter</CustomLink>
+                <CustomLink to="/contact" onClick={toggleMenu}>Contact</CustomLink>
+            </ul>
+            <ul className="desktop">
+                {/* Regular links for desktop */}
+                <CustomLink to="/">Home</CustomLink>
+                <CustomLink to="/about">About Us</CustomLink>
+                <CustomLink to="/newsletter">Newsletter</CustomLink>
+                <CustomLink to="/contact">Contact</CustomLink>
+            </ul>
+        </nav>
+    );
 }
 
 function CustomLink({ to, children, ...props}) {
-	const resolvedPath = useResolvedPath(to)
-	const isActive = useMatch({ path: resolvedPath.pathname, end: true })
-	return (
-		<li className={isActive ? "active" : ""}>
-			<Link to={to} {...props}>
-				{children}
-			</Link>
-		</li>
-	)
+    const resolvedPath = useResolvedPath(to);
+    const isActive = useMatch({ path: resolvedPath.pathname, end: true });
+
+    return (
+        <li className={isActive ? "active" : ""}>
+            <Link to={to} {...props}>
+                {children}
+            </Link>
+        </li>
+    );
 }
