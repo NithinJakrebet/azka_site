1ca124780c7ed830855f3351f072e479a1b23389
Nithin Jakrebet <nithinjbet@gmail.com>
2023-12-29 15:18:43 -0700

    final website essentials done

--- src/pages/Newsletter.jsx
+++ src/pages/Newsletter.jsx
@@ -1,10 +1,54 @@
 import React from "react";
 import AnimatedPage from "../components/AnimatedPage";
+import AppearOnScroll from "../components/AppearOnScroll";
+import "../styling/magazine.css"
+
+import pic2022 from "../photos/2022.jpeg";
+import pdf2022 from "../newsletter_pdfs/AZKA2022Example.pdf"
+import pdf2023 from "../newsletter_pdfs/AZKA2023Example.pdf"
+import pic2023 from "../photos/2023.png";
+
+
+
+function Magazine ({ title, imgURL, pdfURL }) {
+    return (
+        <div className="magazine-container">
+          {/* <img
+            className="magazine-image"
+            src={imgURL} alt="year_pic" 
+          /> */}
+          <a 
+            href={pdfURL}
+            target="_blank" 
+            rel="noopener noreferrer" 
+            className="magazine-url"
+          >
+            {title}
+          </a>
+        </div>    
+    )
+}
 
 export default function Newsletter() {
     return (
         <AnimatedPage>
-            <h1>Newsletter</h1>
+            <AppearOnScroll>
+                <Magazine
+                title="2023 Newsletter"
+                imgURL={pic2022}
+                pdfURL={pdf2023}
+                />
+            </AppearOnScroll>
+            <AppearOnScroll>
+                <Magazine
+                title="2022 Newsletter"
+                imgURL={pic2023}
+                pdfURL={pdf2022}
+                />
+            </AppearOnScroll>     
         </AnimatedPage>
     )
 }
+
+
+
