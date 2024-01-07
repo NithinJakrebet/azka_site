7664239b742531327512b4b0c619ac35ef2075e2
Nithin Jakrebet <nithinjbet@gmail.com>
2024-01-07 15:18:52 -0700

    -newsletter updated

--- src/pages/Newsletter.jsx
+++ src/pages/Newsletter.jsx
@@ -4,20 +4,18 @@ import AppearOnScroll from "../components/AppearOnScroll";
 import "../styling/magazine.css"
 import "../styling/pages.css"
 
-import pic2022 from "../photos/2022.jpeg";
-import pdf2022 from "../newsletter_pdfs/AZKA2022Example.pdf"
-import pdf2023 from "../newsletter_pdfs/AZKA2023Example.pdf"
-import pic2023 from "../photos/2023.png";
-
+import pdf2023 from "../newsletter_pdfs/2023 AZKA Newsletter.pdf";
+// import pic2023 from "../photos/2023.png";
+import pic2023 from "../photos/picnic_2023/group.jpg"
 
 
 function Magazine ({ title, imgURL, pdfURL }) {
     return (
         <div className="magazine-container">
-          {/* <img
+          <img
             className="magazine-image"
             src={imgURL} alt="year_pic" 
-          /> */}
+          />
           <a 
             href={pdfURL}
             target="_blank" 
@@ -33,14 +31,13 @@ function Magazine ({ title, imgURL, pdfURL }) {
 export default function Newsletter() {
     return (
         <AnimatedPage>
-          <div className="text_box">Nothing Here Yet!</div>
-            {/* <AppearOnScroll>
+            <AppearOnScroll>
                 <Magazine
                 title="2023 Newsletter"
-                imgURL={pic2022}
+                imgURL={pic2023}
                 pdfURL={pdf2023}
                 />
-            </AppearOnScroll> */}
+            </AppearOnScroll>
             {/* <AppearOnScroll>
                 <Magazine
                 title="2022 Newsletter"
