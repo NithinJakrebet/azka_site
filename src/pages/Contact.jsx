bfb2c62ef61a392fd9e80b898aa38c9b81fec3f2
Nithin Jakrebet <nithinjbet@gmail.com>
2024-09-05 15:02:27 -0700

    feat: update contact page, use material ui

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,31 +1,48 @@
-import React from "react";
 import AnimatedPage from "../components/AnimatedPage";
 import "../styling/pages.css";
-import fb from "../photos/fb_icon.png"
-import ig from "../photos/Instagram_logo.png"
-import tk from "../photos/tiktok.png"
+import fb from "../photos/fb_icon.png";
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
 
-function Contact() {
+const Contact = () => {
     return (
         <AnimatedPage>
-            <div className="text_box2">
-                <h1>Feel free to contact us at the following</h1>
-                <h3>Shreeja Baliga (President)</h3>
-                <h4>shreejapai@yahoo.com</h4>
-                {/* <h3>Person 2 (position)</h3>
-                <h4>Insert Email Here</h4>
-                <h3>Person 3 (position)</h3>
-                <h4>Insert Email Here</h4> */}
+            <Box className="text_box2">
+                <h1>Contact Us!</h1>
+                <h3>Shreeja Baliga: <a href="mailto:shreejapai@yahoo.com">shreejapai@yahoo.com</a> </h3>
+                <Button
+                    sx={{
+                        transition: 'transform 0.3s ease-in-out',
+                        '&:hover': {
+                            transform: 'scale(1.1)',
+                        },
+                        backgroundColor: '#1976d2',
+                        color: '#fff',
+                        padding: '10px 20px',
+                        margin: '10px 0',
+                        borderRadius: '8px',
+                    }}
+                    href="https://form.jotform.com/242000714755145"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Donate to SVK Temple
+                </Button>
                 <h2>Follow our Socials Below</h2>
-                <a href="https://www.facebook.com/groups/2620372688125086"
-                 target="blank"
-                 ><img className="rounded-image" src={ fb }></img></a>
-                {/* <a href="#" target="blank"><img className="rounded-image" src={ ig }></img></a>
-                <a href="#" target="blank"><img className="rounded-image" src={ tk }></img></a> */}
-            </div>
+                <a 
+                    href="https://www.facebook.com/groups/2620372688125086" 
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <img
+                        className="rounded-image"
+                        src={fb}
+                        alt="Facebook icon"
+                    />
+                </a>
+            </Box>
         </AnimatedPage>
     );
-  }
-  
-  export default Contact;
-  
+}
+
+export default Contact;
