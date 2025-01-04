dbc7edf71d14e53e6e43ceb430188798c33a2c04
Nithin Jakrebet <nithinjbet@gmail.com>
2025-01-03 22:44:16 -0700

    feat: update data

--- frontend/src/data/galleryData.jsx
+++ frontend/src/data/galleryData.jsx
@@ -28,8 +28,25 @@ import sign from "../photos/papago_hike/sign.jpg";
 import FMSC from "../photos/FMSC Mesa- May 3rd.png"
 import chudi from "../photos/AZ Konkanis Chudi puja held on Aug 25th 2024.png"
 
+
+
 // Data structure to hold album info
 export const ALBUMS = [
+    {
+        title: "Diwali 2024",
+        images: [
+            "https://lh3.googleusercontent.com/pw/AP1GczOTMRJfHiZFWFQQqi6dsjEnbXwubdzq-A6Le4iwDSP2VGSyRmX2jDu8cdgXI61cwmOrezxblQaC3dy9yCQYPD5W6jue8oc98XDYw-8025XpAJoeWtM8StxoXkwOl0hWdX1SllPOGjEv3XdiiXWkvAApWg=w1437-h958-s-no-gm?authuser=0",
+            "https://lh3.googleusercontent.com/pw/AP1GczPy-904c55aa8gFcySZt5Rh_3g7zNfIpa6I6D8PW2Oebr7XkDZZRcFYzffwCuRSQX4hZJ_LFCKMLYxM4UKYESBYnmfgGyGiTcagOSIxDmxqx7klZJDQjPM8sUWjvf_vPXoH98fhCV-4XivSCHGDm7t-Tw=w1437-h958-s-no-gm?authuser=0",
+            "https://lh3.googleusercontent.com/pw/AP1GczO4PpMp2Y4_vKgqsveOPWtpkV7RqKGo8MQZlAJKIi_PIL4ZbeSu8_y3K021eeXp_mKqlaRFURgL5BCzZUdOLCJVjItLGVhLHg0eVOhCwDyHgUmWlJ0QYQOkhJJ1oMEvCuyCvRGlgrOroKB3nIgNvUtojA=w1437-h958-s-no-gm?authuser=0",
+            "https://lh3.googleusercontent.com/pw/AP1GczN712bQzYeYPFKrWmCtn4Rdx6eHyzbMzRYjn7W8p03NG2iydUHn5pBsRnrxhFW_W7HClK65U8f5b6gcptTJyRhsjokeVpHegksxRQALUAvTlvW8jVSpmrjkjFInyPP4stWbyR-DHUwTiS0V3cYhUknfNg=w1437-h958-s-no-gm?authuser=0",
+            "https://lh3.googleusercontent.com/pw/AP1GczMux3xENmUeqY5VCHJefRr8QXC727UI25grUKWLlbBUGSHN5HjVtCPJ4pBm-QjOK0phY0UdYoEUIVxkwKiut3_7NcQI06To3WpHxn1gYlSvjli0gv2m0MwFtmgm8xFlKVH5DMH1uXJ_9zYlDnjcrKXJnA=w1437-h958-s-no-gm?authuser=0",
+            "https://lh3.googleusercontent.com/pw/AP1GczMQNSJqXDTAMY6JzDm9x2DA34YN2sDaAp2REB84CauxGodqwLmTxSDwpvgjcDq1ad7QJ4a09QuBaLJQxcYflaUcHpsiyrMrupAff0dz-IMJc16Afxgr_CxVjZ132YXNsSXUyYSY6iUxin3n-0he7YZ1Aw=w1437-h958-s-no-gm?authuser=0",
+            "https://lh3.googleusercontent.com/pw/AP1GczNlGUU77FPXHf3NEXpP1TtDFKpCq_nQD7heSYJxM4YOhnNPzZeiAodc2NIxSc-QmJHzvUNvzvTnNUKC8SmQFk6KbkzWoEzsiixc3U9FrrByAtGGJe76KmnElnNcbdam2C5ElCiTfOB6vzcUVElTbjLYbg=w1437-h958-s-no-gm?authuser=0",
+            "https://lh3.googleusercontent.com/pw/AP1GczPv6uYlr500YZCLWOwUzmiKO4BHUv9fiMq06RJpNGvzVfgIXUwsOwPKaO0ANEvBL2lujmhL7-YNFtxH0gefT1oe3TCekjgdUwFAlPEwZx_meKzDhEngXPMRI7WNgD2lfRQkpXvpmGAUYptQcemoKnvDgg=w1437-h958-s-no-gm?authuser=0",
+
+        ],
+    },
+
     {
         title: "Chudi Puja",
         images: [chudi],
