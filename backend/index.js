2f0e4d5e6b56603f2a71e034d8aca5e9851e4af7
Nithin Jakrebet <nithinjbet@gmail.com>
2025-01-12 16:38:18 -0700

    setup with mongodb and env files

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,35 @@
 // backend/index.js
-require('dotenv').config();
-const express = require('express');
-const connectDB = require('./config/db');
+import dotenv from 'dotenv';
+import express from "express";
+import mongoose from 'mongoose';
+
+dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5555;
+const MONGODBURL = process.env.MONGODBURL
 
-// Connect to DB
-connectDB();
 
-// Middleware
-app.use(express.json());
+app.get('/', (request, response) => {
+  console.log(request);
+  return response.status(234).send('INIT ')
+})
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+
+
+
+mongoose
+  .connect(MONGODBURL)
+
+  .then(() => {
+    console.log(`Connected to DB`)
+
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+
+  .catch((error) => {
+    console.log(error);
+  })
+
